edf5d3090a73f2d351ec094a5b039cb55b80412d
MandadiSriHarsha <venkatharshachowdary@gmail.com>
2023-04-05 06:52:12 +0000

    built a super over league card using react js

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="cover-page">
+    <h1 className="cover-page-heading">Super Over League</h1>
+    <div className="cover-page-image-card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="cover-page-image-card-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="cover-page-image-card-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
